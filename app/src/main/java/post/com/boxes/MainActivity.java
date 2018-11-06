dae4609c3326b8900a2f5c3ecd13a8767b534f71
elisapig <elisa1001940@gmail.com>
2018-11-06 15:51:05 +0800

    exam

--- app/src/main/java/post/com/boxes/MainActivity.java
+++ app/src/main/java/post/com/boxes/MainActivity.java
@@ -1,13 +1,46 @@
 package post.com.boxes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edLength;
+    private EditText edWidth;
+    private EditText edHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
     }
+
+    private void findViews() {
+        edLength = findViewById(R.id.ed_length);
+        edWidth = findViewById((R.id.ed_width));
+        edHeight = findViewById((R.id.ed_height));
+    }
+    public void check(View view){
+    float lenght = Float.parseFloat(edLength.getText().toString());
+    float widtht = Float.parseFloat(edWidth.getText().toString());
+    float height = Float.parseFloat(edHeight.getText().toString());
+     if(lenght<=23&&widtht<=14&&height<=13) {
+         Intent intent = new Intent(this, Box3.class);
+         intent.putExtra("lenght", lenght);
+         intent.putExtra("widtht", widtht);
+         intent.putExtra("widtht", widtht);
+         startActivity(intent);
+     }else{
+         Intent intent = new Intent(this, Box5.class);
+         intent.putExtra("lenght", lenght);
+         intent.putExtra("widtht", widtht);
+         intent.putExtra("widtht", widtht);
+         startActivity(intent);
+
+     }
+}
 }
